776264c95ae2f0bfcaf029f391babd61425d9470
canilgu <canilguu@gmail.com>
2024-06-21 19:31:13 +0300

    .env file used in project

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
+import { API_URL } from '@env'; // Import the environment variable
 
-const API_BASE_URL = Platform.OS === 'ios' ? 'https://canilgu.dev/makyaj-api' : 'https://canilgu.dev/makyaj-api';
+const API_BASE_URL = Platform.OS === 'ios' ? API_URL : API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL, // API base URL
